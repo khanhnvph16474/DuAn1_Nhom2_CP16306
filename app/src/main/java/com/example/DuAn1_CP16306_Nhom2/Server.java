671b40e6df411fb048df182b2eb7a83f8cc04f52
thinhnvph16607 <thinhnvph16607@fpt.edu.vn>
2021-12-11 14:29:39 +0700

    sever

--- app/src/main/java/com/example/DuAn1_CP16306_Nhom2/Server.java
+++ app/src/main/java/com/example/DuAn1_CP16306_Nhom2/Server.java
@@ -1,7 +1,7 @@
 package com.example.DuAn1_CP16306_Nhom2;
 
 public class Server {
-    public static String Food = "https://chuanno1.000webhostapp.com/duan1.json";
-    public  static String User ="https://chuanno1.000webhostapp.com/user.json";
-    public static String Notifi = "https://chuanno1.000webhostapp.com/Notice.json";
+    public static String Food = "https://thinh01.000webhostapp.com/home.json";
+    public  static String User ="https://thinh01.000webhostapp.com/user.json";
+    public static String Notifi = "https://thinh01.000webhostapp.com/notice.json";
 }
